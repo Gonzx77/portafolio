d0e164a854eb1e0dd6080c129d5c24c1d68c82e6
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-10-23 18:32:49 -0500

    style: :lipstick: Ajustes de diseño

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-const miDiv = document.querySelector('#cursorDiv');
+const cursorDiv = document.querySelector('#cursorDiv');
+const cursorShadow = document.querySelector('#cursorDivShadow');
+const particulas = document.querySelector('#particulas');
 
 let targetX = 0;
 let targetY = 0;
 const delay = 0.2;
+const delayS = 0.05;
 
 document.addEventListener('mousemove', (event) => {
-    targetX = event.clientX - 0;
-    targetY = event.clientY - 0;
+    targetX = event.clientX - 2;
+    targetY = event.clientY - 2;
 });
 
 function actualizarPosicion() {
-    const rectMiDiv = miDiv.getBoundingClientRect();
+
+    const rectMiDiv = cursorDiv.getBoundingClientRect();
+    const rectShadow = cursorShadow.getBoundingClientRect();
+
     const currentX = rectMiDiv.left;
     const currentY = rectMiDiv.top;
+
+    const shadowX = rectShadow.left;
+    const shadowY = rectShadow.top;
 
     const diffX = targetX - currentX;
     const diffY = targetY - currentY;
 
-    miDiv.style.left = currentX + diffX * delay + 'px';
-    miDiv.style.top = currentY + diffY * delay + 'px';
+    const diffShadowX = targetX - shadowX;
+    const diffShadowY = targetY - shadowY;
+
+    cursorShadow.style.left = shadowX + diffShadowX * delayS + 'px';
+    cursorShadow.style.top = shadowY + diffShadowY * delayS + 'px';
+
+    
+    cursorDiv.style.left = currentX + diffX * delay + 'px';
+    cursorDiv.style.top = currentY + diffY * delay + 'px';
 
     requestAnimationFrame(actualizarPosicion);
 }
